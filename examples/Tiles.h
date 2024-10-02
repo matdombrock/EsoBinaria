5ff652a2a6d31f800f5ded933f93c3b822d90469
matdombrock <dombrock.mathieu@gmail.com>
2024-10-01 21:11:52 -0700

    cellSprites

--- examples/Tiles.h
+++ examples/Tiles.h
@@ -30,6 +30,40 @@ std::map<std::string, Color> colors = {
     {"WHITE", Color(255,255,255)},
 };
 
+namespace cellSprites {
+    Sprite baseTile = Sprite(Vec2i(0, 0), Vec2i(16, 16), Vec2i(_g.cellSize, _g.cellSize));
+    Sprite blankTile = Sprite(Vec2i(0, 32), Vec2i(16, 16), Vec2i(_g.cellSize, _g.cellSize));
+    Sprite andTile  = Sprite(Vec2i(0, 16), Vec2i(16, 16), Vec2i(_g.cellSize, _g.cellSize));
+    Sprite orTile = Sprite(Vec2i(16, 16), Vec2i(16, 16), Vec2i(_g.cellSize, _g.cellSize));
+    Sprite notTile = Sprite(Vec2i(32, 16), Vec2i(16, 16), Vec2i(_g.cellSize, _g.cellSize));
+    Sprite xorTile = Sprite(Vec2i(48, 16), Vec2i(16, 16), Vec2i(_g.cellSize, _g.cellSize));
+    Sprite nandTile = Sprite(Vec2i(64, 16), Vec2i(16, 16), Vec2i(_g.cellSize, _g.cellSize));
+    Sprite norTile = Sprite(Vec2i(80, 16), Vec2i(16, 16), Vec2i(_g.cellSize, _g.cellSize));
+    Sprite xnorTile = Sprite(Vec2i(96, 16), Vec2i(16, 16), Vec2i(_g.cellSize, _g.cellSize));
+    Sprite parenR = Sprite(Vec2i(32, 0), Vec2i(16, 16), Vec2i(_g.cellSize, _g.cellSize));
+    Sprite inATile = Sprite(Vec2i(0, 48), Vec2i(16, 16), Vec2i(_g.cellSize, _g.cellSize));
+    Sprite inBTile = Sprite(Vec2i(16, 48), Vec2i(16, 16), Vec2i(_g.cellSize, _g.cellSize));
+    Sprite inCTile = Sprite(Vec2i(32, 48), Vec2i(16, 16), Vec2i(_g.cellSize, _g.cellSize));
+    Sprite inDTile = Sprite(Vec2i(48, 48), Vec2i(16, 16), Vec2i(_g.cellSize, _g.cellSize));
+    Sprite Void = Sprite(Vec2i(16, 32), Vec2i(16, 16), Vec2i(_g.cellSize, _g.cellSize));
+    std::map <CellType, Sprite*> cellMap = {
+        {CT_EMPTY, &blankTile},
+        {CT_AND, &andTile},
+        {CT_OR, &orTile},
+        {CT_NOT, &notTile},
+        {CT_XOR, &xorTile},
+        {CT_NAND, &nandTile},
+        {CT_NOR, &norTile},
+        {CT_XNOR, &xnorTile},
+        {CT_BLANK, &blankTile},
+        {CT_INA, &inATile},
+        {CT_INB, &inBTile},
+        {CT_INC, &inCTile},
+        {CT_IND, &inDTile},
+        {CT_VOID, &Void}
+    };
+};
+
 enum CellType {
     CT_VOID = -1,
     CT_EMPTY = 0,
