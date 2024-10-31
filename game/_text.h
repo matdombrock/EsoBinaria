3e350b25d4ebfa6c76dd21fdabe0557bc7cfe7e8
matdombrock <dombrock.mathieu@gmail.com>
2024-10-31 04:18:09 -0700

    update text

--- game/_text.h
+++ game/_text.h
@@ -10,7 +10,7 @@ You have been accepted into the <$FF00FF$>EsoBinaria Research Program<$FFFFFF$>.
 Our goal here is to <$FF0000$>[REDACTED]<$FFFFFF$> the EsoBinaria device.
 To do this we must understand how it works.
 We have a tome documenting its inputs and outputs but no instructions for how to build the device.
-You aim is to recreate small parts of the mechanism via reverse-<$FF0000$>[REDACTED]<$FFFFFF$>.
+Your aim is to recreate small parts of the mechanism via reverse-<$FF0000$>[REDACTED]<$FFFFFF$>.
 <$00FF00$>
 Mission Statement:
 <$FFFFFF$>
