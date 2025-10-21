43e3edb78bcbf6145bb580dd906a9c9f081c9ebb
matdombrock <dombrock.mathieu@gmail.com>
2025-10-20 19:44:54 -0700

    fix builds

--- build.wasm.sh
+++ build.wasm.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p buildwasm
 
 # Build s7
-if [ ! -f ./build/s7.o ]; then
+if [ ! -f ./buildwasm/s7.wasm.o ]; then
   echo "Compiling s7 for wasm..."
   emcc -c ./lib/s7/s7.c -o ./buildwasm/s7.wasm.o -std=c11
 else
@@ -14,5 +14,6 @@ fi
 
 # Build the project
 cd buildwasm
+echo `which emcmake`
 emcmake cmake -DESO_WASM=ON ..
 make
---
+++ run.wasm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -e
+
+npx http-server ./buildwasm/out
